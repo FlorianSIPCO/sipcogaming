3ce0f43b4cdcedf6201b827fd341fc259481d93c
Florian-Bclt <bouclet.florian@hotmail.com>
2025-03-20 15:54:29 +0100

    edit metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SIPCO Gaming | PC Gamer sur-mesure & Performances optimisées",
   description: "SIPCO Gaming vous propose des PC Gamer assemblés sur-mesure avec des composants de dernière génération pour une performance inégalée.",
-  metadataBase: new URL('https://sipco-gaming.fr'),
+  metadataBase: new URL('https://sipcogaming.vercel.app/'),
   keywords: "PC Gamer, PC sur-mesure, Montage PC, Composants gaming, SIPCO Gaming, Performance PC, Ordinateur de jeu, SIPCO, Informatique, Vitré, Bretagne, Ile-et-Vilaine, Ile et Vilaine, ",
   robots: "index, follow", // Google
   openGraph: { // Facebook, LinkedIn, Discord
     type: "website",
     title: "SIPCO Gaming | PC Gamer sur-mesure & Performances optimisées",
     description: "Découvrez nos PC Gamer performants montés sur-mesure avec des composants haut de gamme.",
-    url: "https://www.sipcogaming.com", // Confirmer URL
+    url: "https://sipcogaming.vercel.app/", // Confirmer URL
     images: [
       {
         url: "/images/logo.jpg", // Image de prévisualisation pour les réseaux sociaux
