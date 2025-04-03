4f6cf8c463088d17a6889248eecb1ea0f592b7ef
Florian-Bclt <bouclet.florian@hotmail.com>
2025-04-03 17:54:00 +0200

    add cookies

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import CookieBanner from "./CookieBanner";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -5,6 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="antialiased flex flex-col min-h-screen">
       <Navbar />
+      <CookieBanner />
       <main className="flex flex-col flex-grow">{children}</main>
       <Footer/>
     </div>
