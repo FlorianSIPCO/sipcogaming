33e15cfa60372a1fc210b9209f3d8e75d1913d9c
Florian-Bclt <bouclet.florian@hotmail.com>
2025-05-12 15:23:56 +0200

    maj stripe

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { sendMail } from "@/lib/sendMail";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-02-24.acacia",
+  apiVersion: "2025-04-30.basil",
 });
 
 export async function POST(req: NextRequest) {
