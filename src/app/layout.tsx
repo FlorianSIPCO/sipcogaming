1b502039f2dfda425883dcaff0a82ca2c048271d
Florian-Bclt <bouclet.florian@hotmail.com>
2025-05-15 15:41:52 +0200

    update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,17 +21,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SIPCO Gaming | PC Gamer sur-mesure & Performances optimisées",
   description: "SIPCO Gaming vous propose des PC Gamer assemblés sur-mesure avec des composants de dernière génération pour une performance inégalée.",
-  metadataBase: new URL('https://sipcogaming.vercel.app/'),
+  metadataBase: new URL('https://sipcogaming.fr/'),
   keywords: "PC Gamer, PC sur-mesure, Montage PC, Composants gaming, SIPCO Gaming, Performance PC, Ordinateur de jeu, SIPCO, Informatique, Vitré, Bretagne, Ile-et-Vilaine, Ile et Vilaine, ",
   robots: "index, follow", // Google
   openGraph: { // Facebook, LinkedIn, Discord
     type: "website",
+    siteName: "SIPCO Gaming",
     title: "SIPCO Gaming | PC Gamer sur-mesure & Performances optimisées",
     description: "Découvrez nos PC Gamer performants montés sur-mesure avec des composants haut de gamme.",
-    url: "https://sipcogaming.vercel.app/", // Confirmer URL
+    url: "https://sipcogaming.fr/", // Confirmer URL
     images: [
       {
-        url: "/images/logo.jpg", // Image de prévisualisation pour les réseaux sociaux
+        url: "https://sipcogaming.fr/images/logo.jpg", // Image de prévisualisation pour les réseaux sociaux
         width: 1200,
         height: 630,
         alt: "SIPCO Gaming - PC Gamer haut de gamme",
@@ -42,7 +43,13 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "SIPCO Gaming | PC Gamer sur-mesure & Performances optimisées",
     description: "Découvrez nos PC Gamer performants montés sur-mesure avec des composants haut de gamme.",
-    images: ["/images/logo.jpg"],
+    images: ["https://sipcogaming.fr/images/logo.jpg"],
+  },
+  alternates: {
+    canonical: "https://sipcogaming.fr/",
+    languages: {
+      'fr-FR': 'https://sipcogaming.fr'
+    }
   },
 };
 
